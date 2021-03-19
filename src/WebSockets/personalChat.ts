0fc3a09c295c5dcdebe0292aa4057548ebe61d67
woodburydev <54697770+woodburydev@users.noreply.github.com>
2021-03-19 09:55:09 -0700

    Typescript Upgrades (#2)
    * Main Conversion to Typescript!
    
    * Small Updates
    
    * Migration to Typescript, Added Interfaces for Messages.
    
    * Socket Definition Baby
    
    Co-authored-by: Nathaniel Woodbury <nate_woodbury@trendmicro.com>

--- src/WebSockets/personalChat.ts
+++ src/WebSockets/personalChat.ts
@@ -1,42 +1,51 @@
+import { Socket } from "socket.io";
+import { PrivateChatSessionUser, PrivateChatMessage, JoinedMessage } from "./types";
+
 const socketio = require("socket.io");
 
-export default function(server) {
-    const io = socketio(server, {
-        cors: {
-        origin: "http://localhost:3000",
-        },
-    });
-    
-    let users: any = [];
+export default function (server) {
+  const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
 
-    io.on("connect", (socket) => {
-        console.log("user has connected");
-        socket.on("join", ({ name, friend }) => {
-          users.push({ socketId: socket.id, name, friend });
-        });
-      
-        socket.on("message", ({ friend, message }) => {
-          const personalUser = users.find((user) => user.socketId === socket.id);
-      
-          const actualReciepientObject = users.find((user) => user.name === friend);
-          if (actualReciepientObject) {
-            io.to(actualReciepientObject.socketId).emit("message", {
-              sentBy: personalUser.name,
-              message,
-            });
-          }
-      
-          io.to(personalUser.socketId).emit("message", {
+  let users: PrivateChatSessionUser[] = [];
+
+  io.on("connect", (socket: Socket) => {
+    console.log("user has connected");
+    socket.on("join", ({ name, friend }: JoinedMessage) => {
+      users.push({ socketId: socket.id, name, friend });
+    });
+
+    socket.on("message", ({ friend, message }: PrivateChatMessage) => {
+
+      const personalUser = users.find((user) => user.socketId === socket.id);
+      const actualReciepientObject = users.find((user) => user.name === friend);
+
+      if (personalUser) {
+        if (actualReciepientObject) {
+          io.to(actualReciepientObject.socketId).emit("message", {
             sentBy: personalUser.name,
             message,
           });
+        }
+        io.to(personalUser.socketId).emit("message", {
+          sentBy: personalUser.name,
+          message,
         });
-      
-        socket.on("disconnect", () => {
-          console.log("user has left");
-          users = users.filter((item) => {
-            return item.socketId !== socket.id;
-          });
-        });
+      }
     });
+
+    socket.on("disconnect", () => {
+      console.log("user has left");
+      users = users.filter((item) => {
+        return item.socketId !== socket.id;
+      });
+    });
+
+    socket.on('end', function () {
+      socket.disconnect();
+    });
+  });
 }
---
+++ src/WebSockets/types.ts
@@ -0,0 +1,15 @@
+export interface PrivateChatSessionUser {
+    socketId: string;
+    name: string;
+    friend: string;
+}
+
+export interface PrivateChatMessage {
+    friend: string;
+    message: string;
+}
+
+export interface JoinedMessage {
+    name: string;
+    friend: string;
+}
