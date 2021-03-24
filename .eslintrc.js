022953c35925698b7667204c26d205f838891927
woodburydev <54697770+woodburydev@users.noreply.github.com>
2021-03-23 22:34:36 -0700

    SERIOUS UPGRADE MANNNNNN (#10)
    * upgrade 1
    
    * upgrade 2
    
    * Revert
    
    * use const

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     ],
     "rules": {
         "@typescript-eslint/no-var-requires": 0,
-        "@typescript-eslint/no-explicit-any": 0
+        "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-non-null-assertion": 0
     }
 };
--- src/WebSockets/personalChat.ts
+++ src/WebSockets/personalChat.ts
@@ -1,7 +1,11 @@
 import { Socket } from "socket.io";
-import { PrivateChatSessionUser, JoinedMessage, PrivateChatMessage } from "./types";
-import { Server } from 'http';
-import Message from '../models/message';
+import {
+  PrivateChatSessionUser,
+  JoinedMessage,
+  PrivateChatMessage,
+} from "./types";
+import { Server } from "http";
+import Message from "../models/message";
 
 const socketio = require("socket.io");
 
@@ -15,46 +19,73 @@ export default function (server: Server): any {
   let users: PrivateChatSessionUser[] = [];
 
   io.on("connect", (socket: Socket) => {
-    console.log("user has connected");
     socket.on("join", ({ name, friend }: JoinedMessage) => {
       users.push({ socketId: socket.id, name, friend });
     });
 
     socket.on("message", async ({ friend, message }: PrivateChatMessage) => {
-
+      // Find personal user by socket connection
       const personalUser = users.find((user) => user.socketId === socket.id);
-      const actualReciepientObject = users.find((user) => user.name === friend);
-      
-      if (personalUser) {
-        const socketMessage:any =  {
-          sentBy: personalUser.name,
-          message,
+
+      // Find all sessions of personal user by username
+      const personalUserSessions = users.filter(
+        (user) => personalUser!.name === user.name
+      );
+
+      // Find all recipient sessions by username
+      const recipientSessions = users.filter((user) => user.name === friend);
+
+      const socketMessage: any = {
+        sentBy: personalUserSessions[0].name,
+        message,
+      };
+
+      // Save message to DB
+      const newMessage = new Message({
+        ...socketMessage,
+        recipient: friend,
+      });
+
+      await newMessage.save();
+
+      if (recipientSessions.length > 0) {
+        // Send message to all active recipient sessions
+        for (const item of recipientSessions) {
+          io.to(item.socketId).emit("message", socketMessage);
         }
+      }
 
-        // Save message to DB
-        const newMessage = new Message({
-          ...socketMessage,
-          recipient: friend
-        });
+      // Send message to all active personal user sessions
+      for (const item of personalUserSessions) {
+        io.to(item.socketId).emit("message", socketMessage);
+      }
+    });
 
-        await newMessage.save();
+    socket.on("typing", ({ friend, isTyping }) => {
+      // Find personal user session by socket id
+      const personalUser = users.find((user) => user.socketId === socket.id);
 
-        if (actualReciepientObject) {
-          io.to(actualReciepientObject.socketId).emit("message", socketMessage);
+      // Cop all recipient sessions by username
+      const recipientSessions = users.filter((user) => user.name === friend);
+
+      if (recipientSessions.length > 0) {
+        // Broadcast to all active recipient sessions who is typing, and if they are.
+        for (const item of recipientSessions) {
+          io.to(item.socketId).emit("typing", {
+            personTyping: personalUser!.name,
+            isTyping,
+          });
         }
-
-        io.to(personalUser.socketId).emit("message", socketMessage);
       }
     });
 
     socket.on("disconnect", () => {
-      console.log("user has left");
       users = users.filter((item) => {
         return item.socketId !== socket.id;
       });
     });
 
-    socket.on('end', function () {
+    socket.on("end", function () {
       socket.disconnect();
     });
   });
